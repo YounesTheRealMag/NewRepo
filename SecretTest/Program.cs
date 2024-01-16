5cf35a4b3e976eb2278b3125095c1c916bef532b
XR group - Gijs <156791842+YounesTheRealMag@users.noreply.github.com>
2024-01-16 13:43:54 +0100

    t

--- SecretTest/Program.cs
+++ SecretTest/Program.cs
@@ -4,8 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Op zoek naar secret...");
+                var mySecret = Environment.GetEnvironmentVariable("CONNECT_WEBSOCKET", EnvironmentVariableTarget.Process);
+                var tt = Environment.GetEnvironmentVariables();
 
+                if (mySecret == null)
+                {
+                    Console.WriteLine("Secret is null.");
+                }
+                else
+                {
+                    Console.WriteLine($"Secret: {mySecret}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Uitzondering gevangen: {ex.Message}");
+            }
+
+            Console.WriteLine("Druk op Enter om af te sluiten...");
+            Console.ReadLine();
         }
     }
 }
